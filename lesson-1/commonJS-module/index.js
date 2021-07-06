8ad253b0ad7d0b6e7073b252563e66bd92f945c7
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-07-06 18:43:00 +0300

    add lesson 1

---
+++ lesson-1/commonJS-module/index.js
@@ -0,0 +1,13 @@
+const users = require("./users");
+
+console.log(users);
+
+// const {admins} = require("./users");
+//
+// const {getCurrentMonth} = require("./date");
+//
+// console.log(`Сейчас ${getCurrentMonth()} месяц`);
+//
+// const currentMonth = require("./date/getCurrentMonth")();
+//
+// console.log(`Сейчас ${currentMonth} месяц`);
